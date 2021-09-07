f0857cf9ce475689217f7558dafc9d5c104dd6b7
a-k-r-e-c-h <itz.a.k.r.e.c.h@gmail.com>
2021-09-07 22:11:29 +0600

    Update and rename README.md to akrech.sh

--- README.md
+++
@@ -1,1 +0,0 @@
-# akrech-rev-shell
---
+++ akrech.sh
@@ -0,0 +1,26 @@
+# akrech-rev-shell   ---a simple metasploit automated reverse-shell-listener tool written in bash.
+#this tool is made for time saving & day-to-day usage specifically for tryhackme users.
+#metasploit has must be installed to ran this script on your machine.
+#found me on tweeter-- https://twitter.com/a_k_r_e_c_H
+
+#!/bin/bash
+
+ 
+echo -e " \e[91m
+
+
+░█████╗░██╗░░██╗██████╗░███████╗░█████╗░██╗░░██╗
+██╔══██╗██║░██╔╝██╔══██╗██╔════╝██╔══██╗██║░░██║
+███████║█████═╝░██████╔╝█████╗░░██║░░╚═╝███████║
+██╔══██║██╔═██╗░██╔══██╗██╔══╝░░██║░░██╗██╔══██║
+██║░░██║██║░╚██╗██║░░██║███████╗╚█████╔╝██║░░██║
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝"
+echo -e " \e[5m
+                       git- a-k-r-e-c-h
+                       mail- itz.a.k.r.e.c.h@gmail.com
+                      "
+
+msfconsole -q -x " use exploit/multi/handler;    
+  set lhost tun0 ;
+   set lport 4444 ;
+    exploit ;"
