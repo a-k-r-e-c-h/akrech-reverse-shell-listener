f99b2bddd330d54fbe6ac92ded5f541360e12041
akrech <itz.a.k.r.e.c.h@gmail.com>
2021-09-08 04:54:32 -0400

    Update akrech.sh

--- akrech.sh
+++ akrech.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # akrech-rev-shell   ---a simple metasploit automated reverse-shell-listener tool written in bash.
 #this tool is made for time saving & day-to-day usage specifically for tryhackme users.
 #metasploit has must be installed to ran this script on your machine.
 #found me on tweeter-- https://twitter.com/a_k_r_e_c_H
 
-#!/bin/bash
+
 
  
 echo " \e[91m
