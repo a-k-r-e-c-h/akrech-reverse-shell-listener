1d2b8d7b5ba0cde9c0a0457a4ba6fd7b423c3bcc
akrech <itz.a.k.r.e.c.h@gmail.com>
2021-09-08 05:05:04 -0400

    Update akrech.sh

--- akrech.sh
+++ akrech.sh
@@ -3,9 +3,6 @@
 # akrech-rev-shell   ---a simple metasploit automated reverse-shell-listener tool written in bash.
 #this tool is made for time saving & day-to-day usage specifically for tryhackme users.
 #metasploit has must be installed to ran this script on your machine.
-#found me on tweeter-- https://twitter.com/a_k_r_e_c_H
-
-
 
  
 echo -e " \e[91m
@@ -18,8 +15,9 @@ echo -e " \e[91m
 ██║░░██║██║░╚██╗██║░░██║███████╗╚█████╔╝██║░░██║
 ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝"
 echo -e " \e[5m
-                       git- a-k-r-e-c-h
-                       mail- itz.a.k.r.e.c.h@gmail.com
+                       git-      a-k-r-e-c-h
+                       mail-     itz.a.k.r.e.c.h@gmail.com
+                       tweeter-  a_k_r_e_c_H
                       "
 
 msfconsole -q -x " use exploit/multi/handler;    
